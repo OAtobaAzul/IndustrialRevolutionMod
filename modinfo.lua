36373735502f30321d1915968c893d8578636cca
OAtobaAzul <85000121+OAtobaAzul@users.noreply.github.com>
2021-07-18 19:01:49 -0300

    Config

--- modinfo.lua
+++ modinfo.lua
@@ -13,4 +13,49 @@ icon = "preview.tex"
 server_filter_tags = {"tech","winona","automation"}
 priority = 10
                
-configuration_options = {}
+configuration_options = {
+    {
+        name = "SPOTLIGHT_BUFF_TEMP",
+        label = "Buff Spotlight?",
+        options =    {
+                        {description = "False", data = 0},
+                        {description = "True",   data = 1},
+                    },
+        default = 1,
+        hover = "Choose wheter to buff the Spotlight"
+   
+ },
+
+        name = "WALL_BUFF",
+        label = "Buff Walls?",
+        options =    {
+                        {description = "False", data = 0},
+                        {description = "True",   data = 1},
+                    },
+        default = 1,
+        hover = "Choose wheter to buff Walls"
+   
+ },
+    {
+        name = "CATAPULT_BUFF_TEMP",
+        label = "Buff Catapult?",
+        options =    {
+                        {description = "False", data = 0},
+                        {description = "True",   data = 1},
+                    },
+        default = 1,
+        hover = "Choose wheter to buff the Catapult"
+   
+ },
+{
+        name = "WINONA_REWORK", --todo: split it more.
+        label = "Enable the Winona rework?",
+        options =    {
+                        {description = "False", data = 0},
+                        {description = "True",   data = 1},
+                    },
+        default = 1,
+        hover = "Choose wheter to Enable the Winona rework"
+   
+ },
+}
